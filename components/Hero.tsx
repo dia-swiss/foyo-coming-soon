d50e4bd374290f0b351271a362f6d49e599f2650
Mauro Balbi <maurobalbi@bluewin.ch>
2021-12-22 22:43:32 +0100

    Try padding

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
           layout='fill'
         />
       </div>
-      <div className="flex flex-col relative mx-4 min-h-screen justify-center text-center">
+      <div className="flex flex-col relative mx-4 py-30 min-h-screen justify-center text-center">
         <p className="font-bold py-10 text-5xl text-white border-b-2 border-white">qoyo.io</p>
         <h1 className="text-2xl text-white py-5">In 15min zur digitalen Marketingstrategie fur KMU, Selbständige und Startups</h1>
         <p className="text-white py-2">Aktuell ist qoyo.io im Aufbau. Sobald das Marketing-Tool zur Verfügung steht (voraussichtlich im März 2022), können Sie einfach und schnell über das Beantworten von ca. 30 Fragen erfahren, WARUM Sie WAS im digitalen Marketing machen müssen, um neue Kunden zu gewinnen. </p>
