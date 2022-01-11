b8ce0596c97952eb3a2cf35450e446ca30f8b52a
Mauro Balbi <maurobalbi@bluewin.ch>
2022-01-11 18:15:43 +0100

    rewrite rules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/internal/:path*',
+        destination: 'http://46.101.209.133/:path*'
+      },
+      {
+        source: '/internal/:path*/',
+        destination: 'http://46.101.209.133/:path*/'
+      }
+    ]
+  }
 }
